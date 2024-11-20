9775ce999a58fd61709a71e80e17dfc5d407d9c7
Antonio Estela <antonio.otalora@ticketmaster.com>
2024-11-20 15:18:48 -0500

    feat: remove hardcoding values

--- src/app/server-lib/utils.ts
+++ src/app/server-lib/utils.ts
@@ -1,4 +1,3 @@
-import COMMANDS from '@/lib/constants';
 import net from 'net';
 import { NextResponse } from 'next/server';
 
@@ -9,33 +8,6 @@ export async function fetchRealTimeData(host: string, port: number, command: str
 
     // Convert command from hexadecimal string to Buffer
     const commandBuffer = Buffer.from(command.replace(/\s+/g, ''), 'hex');
-
-    if (process.env.NODE_ENV === 'development') {
-      let response;
-      console.log(`Command: ${command}`);
-      switch (command) {
-        case COMMANDS.QUERY_POWER_STATUS:
-          console.log('Querying power status');
-          response = '05 14 00 03 00 00 00 02 19';
-          break;
-        case COMMANDS.GET_TEMPERATURE:
-          console.log('Querying temperature');
-          response = '05 14 00 0A 00 00 00 B6 03 00 00 00 00 00 00 48';
-          break;
-        case COMMANDS.GET_LAMP_HOURS:
-          console.log('Querying lamp hours');
-          response = '05 14 00 06 00 00 00 B8 0B 00 00 DD';
-          break;
-        default:
-          console.log('Unknown command');
-      }
-      resolve(
-        NextResponse.json({
-          status: 'success',
-          response,
-        })
-      );
-    }
 
     try {
       // Connect to the device
